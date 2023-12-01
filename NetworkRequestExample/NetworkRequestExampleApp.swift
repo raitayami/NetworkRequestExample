f7df45f59a07ddf954586aac435a41b3beac30fb
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-12-01 09:46:30 +0000

    Initial Commit

---
+++ NetworkRequestExample/NetworkRequestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkRequestExampleApp.swift
+//  NetworkRequestExample
+//
+//  Created by Tayami Rai on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkRequestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
